de4b18424f786ba0107ac06efec96fe0997fb7d9
ETHAN2545 <B00164666@mytudublin.ie>
2026-09-23 14:57:28 +0100

    completed lab 1 part 1 using editview to collect information and toast to display the completion message along with the users name

--- Part 1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Part 1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,50 +1,33 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.navigation.fragment.NavHostFragment;
-
-import com.example.myapplication.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
 
-    public void submitButton_click(View view) {
+    public void button_click(View view) {
 
-        TextView tv = (TextView) findViewById(R.id.button);
-        TextView text = (TextView) findViewById(R.id.textView7);
-        tv.setText("Thank you, your request is being processed!");
+        EditText editText = (EditText)findViewById(R.id.name);
+        Toast.makeText(this, "Thank you " + editText.getText() + ", your request is being processed!",Toast.LENGTH_LONG).show();
     }
 }
